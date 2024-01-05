27964f15d036bd6bf1e29a1fdf00e7dff41cea35
huxw <huxw@mail.jj.cn>
2024-01-05 09:40:00 +0800

    使用extra

--- src/util/puppeteer.ts
+++ src/util/puppeteer.ts
@@ -1,18 +1,27 @@
 
-import puppeteer, { PuppeteerLaunchOptions } from "puppeteer";
+import { PuppeteerLaunchOptions } from "puppeteer";
 import _ from "lodash";
+
+import puppeteer from 'puppeteer-extra'
+import StealthPlugin from 'puppeteer-extra-plugin-stealth'
+
 
 const DEFAULT_CREATE_OPTIONS: PuppeteerLaunchOptions = {
     headless: true,
     timeout: 3000,
     slowMo: 10,
+    defaultViewport: null,
     ignoreDefaultArgs: ['--enable-automation', '--disable-extensions'],
-    args: ['--no-sandbox',
+    args: [
+        '--start-maximized',
+        '--no-sandbox',
         '--disable-setuid-sandbox',
         '--disable-features=site-per-process',
         "--disable-web-security"
     ]
 }
+
+puppeteer.use(StealthPlugin());
 export async function createPage(options: PuppeteerLaunchOptions = {}) {
     const browser = await puppeteer.launch(_.merge(DEFAULT_CREATE_OPTIONS, options));
     // 新开标签页
